125baf8addcf81a51f16e60b5c5f15243ee35293
kame_mac <kametyaso@gmail.com>
2024-08-03 22:43:11 +0900

    feat: add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root  'static_pages#top'
+  resources :users, only: [:new, :create]
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
