864efaa821ab9aef13eb954778b5e199b00c2cd8
kame_mac <kametyaso@gmail.com>
2024-08-04 21:39:32 +0900

    fix:table schema delete tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_04_031732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
