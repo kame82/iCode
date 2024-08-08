7060a39c408d088efbd92e8f776388b64d0b1c1b
kame_mac <kametyaso@gmail.com>
2024-08-09 02:15:17 +0900

    feat: flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice then 'bg-teal-100 border-teal-500 text-teal-900'
+    when :alert then 'bg-red-100 border-red-400 text-red-700'
+    when :error then 'bg-orange-100 border-orange-500 text-orange-700'
+    else 'bg-gray-500'
+    end
+  end
 end
