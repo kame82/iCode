61e20f84d22ee723ab138af873a45b72105d7eaa
kame_mac <kametyaso@gmail.com>
2024-08-16 13:41:51 +0900

    feat: code delete func.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -25,10 +25,18 @@ class CodesController < ApplicationController
   def update
     @code = Code.find(params[:id])
     if @code.update(code_params)
+      flash[:notice] = t('flash.code.update')
       redirect_to codes_path
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @code = Code.find(params[:id])
+    @code.destroy!
+    flash[:notice] = t('flash.code.destroy')
+    redirect_to codes_path
   end
 
   private
