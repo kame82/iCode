1078ad62484ab6ae4cda6692536d9feb99c03a9b
kame_mac <kametyaso@gmail.com>
2024-08-19 02:26:31 +0900

    fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,15 +11,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # else
   #   storage :file # 開発環境とテスト環境ではfileを使用
   # end
-
-  if Rails.env.development? # 開発環境の場合
-    # storage :file
-    storage :fog
-  elsif Rails.env.test? # テスト環境の場合
-    storage :file
-  else # 本番環境の場合
-    storage :fog
-  end
+storage :aws
+  # if Rails.env.development? # 開発環境の場合
+  #   # storage :file
+  #   storage :fog
+  # elsif Rails.env.test? # テスト環境の場合
+  #   storage :file
+  # else # 本番環境の場合
+  #   storage :fog
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process resize_to_fill: [300, 200]
+  process resize_to_fill: [300, 200]
   #
   # def scale(width, height)
   #   # do something
