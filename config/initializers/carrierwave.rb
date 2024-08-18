1948066198c2b872f1f16d83eee2ab3625dbcb5a
kame_mac <kametyaso@gmail.com>
2024-08-18 19:56:14 +0900

    fix: upload env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.production? && !ENV["AWS_ACCESS_KEY_ID"].nil?  # 本番環境の場合はS3へアップロード
+  if Rails.env.production? && !ENV["AWS_ACCESS_KEY_ID"].nil?  # 本番環境の場合はS3へアップロード
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'myicode' # バケット名
@@ -16,9 +16,9 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1', # リージョン
       path_style: true
     }
-  # else # 本番環境以外の場合はアプリケーション内にアップロード
-  #   config.storage :file
-  #   config.enable_processing = false if Rails.env.test? || Rails.env.ci?
-  # end
+  else # 本番環境以外の場合はアプリケーション内にアップロード
+    config.storage :file
+    config.enable_processing = false if Rails.env.test? || Rails.env.ci?
+  end
   raise "Missing required arguments: aws_access_key_id, aws_secret_access_key" unless ENV['aws_access_key_id'] && ENV['aws_secret_access_key']
 end
