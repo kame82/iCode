921dcb4ddf380d74db77b8c1a697b2e1145c72e6
kame_mac <kametyaso@gmail.com>
2024-08-20 02:06:12 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -44,7 +44,7 @@ require 'carrierwave/storage/fog'
 #     config.enable_processing = false if Rails.env.test? || Rails.env.ci?
 # end
 CarrierWave.configure do |config|
-  if Rails.env.production? && ENV['S3_ACCESS_KEY_ID'].present? && ENV['S3_SECRET_ACCESS_KEY'].present?
+  # if Rails.env.production? && ENV['AWS_ACCESS_KEY_ID'].present? && ENV['AWS_SECRET_ACCESS_KEY'].present?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'myicode'
@@ -52,12 +52,12 @@ CarrierWave.configure do |config|
     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/myicode"
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
       path_style: true
     }
-  else
-    config.storage :file
-  end
+  # else
+  #   config.storage :file
+  # end
 end
