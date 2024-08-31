95ebb32d3e7f5eda882693f7bb619ddca18b66e8
kame_mac <kametyaso@gmail.com>
2024-08-31 11:10:30 +0900

    fix: add editor title's edit mode

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,29 @@ import "@hotwired/turbo-rails";
 import "./controllers";
 
 import "./editor/editor.bundle.js";
+
+const codeTitle = document.getElementById("code_title");
+const codeTitleInput = document.getElementById("code_title_input");
+var defaultValue = codeTitle.textContent;
+
+// タイトルをクリックしたらinputに変更
+codeTitle.addEventListener("click", function () {
+  codeTitle.style.display = "none";
+  codeTitleInput.style.display = "block";
+  codeTitleInput.focus();
+  defaultValue = codeTitle.textContent;
+});
+
+// inputからフォーカスが外れたらタイトルに変更
+codeTitleInput.addEventListener("blur", function () {
+  const codeTitleInputValue = codeTitleInput.value.trim();
+  if (codeTitleInputValue === "") {
+    codeTitleInput.value = defaultValue; // 空白の場合はdefaultValueを代入
+    codeTitle.textContent = defaultValue; // codeTitleもdefaultValueに設定
+  } else {
+    defaultValue = codeTitleInputValue; // 新しい値をdefaultValueに更新
+    codeTitle.textContent = codeTitleInputValue; // 入力値をタイトルに設定
+  }
+  codeTitle.style.display = "inline-block";
+  codeTitleInput.style.display = "none";
+});
