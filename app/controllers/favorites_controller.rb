6da2dd5701a83511e8bc525029b6d40d95a1144a
kame_mac <kametyaso@gmail.com>
2024-09-01 02:19:27 +0900

    fix: favorite controller message translate to jap

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,10 @@ class FavoritesController < ApplicationController
     def create
       @favorite = current_user.favorites.new(code_id: params[:code_id])
       if @favorite.save
-        flash[:notice] = t('flash.favorite.create')
+        flash[:notice] = "いいねしました" #あとで削除する
         redirect_to codes_path
       else
-        flash[:alert] = t('flash.favorite.create_error')
+        flash[:alert] = "いいねに失敗しました" #あとで削除する
         render :edit, status: :unprocessable_entity
       end
     end
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
     def destroy
       @favorite = current_user.favorites.find_by(code_id: params[:code_id])
       @favorite.destroy!
-      flash[:notice] = t('flash.favorite.destroy')
+      flash[:notice] = "いいねを解除しました" #あとで削除する
       redirect_to codes_path
     end
 end
