ca807f16fed766dcbceb4632b4f4010ad0de0ce1
kame_mac <kametyaso@gmail.com>
2024-08-31 11:14:02 +0900

    fix: codes title defaults value

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_17_045653) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_30_161848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "codes", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title", default: "untitled", null: false
     t.text "body_html"
     t.text "body_css"
     t.text "body_js"
