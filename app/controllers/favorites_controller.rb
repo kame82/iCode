a67816837aa2476610a69159a1843182d84d8c93
kame_mac <kametyaso@gmail.com>
2024-09-01 15:21:15 +0900

    feat: favorite func

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,9 @@
 class FavoritesController < ApplicationController
     def create
-      @favorite = current_user.favorites.new(code_id: params[:code_id])
+        @favorite = current_user.favorites.new(code_id: params[:code_id])
       if @favorite.save
-        flash[:notice] = "いいねしました" #あとで削除する
         redirect_to codes_path
       else
-        flash[:alert] = "いいねに失敗しました" #あとで削除する
         render :edit, status: :unprocessable_entity
       end
     end
@@ -13,7 +11,6 @@ class FavoritesController < ApplicationController
     def destroy
       @favorite = current_user.favorites.find_by(code_id: params[:code_id])
       @favorite.destroy!
-      flash[:notice] = "いいねを解除しました" #あとで削除する
       redirect_to codes_path
     end
 end
