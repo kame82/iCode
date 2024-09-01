4739ceaf1f02d7b9f532fe0025940e15d719a089
kame_mac <kametyaso@gmail.com>
2024-09-01 16:26:24 +0900

    fix: redirect action on favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
     def create
         @favorite = current_user.favorites.new(code_id: params[:code_id])
       if @favorite.save
-        redirect_to codes_path
+          redirect_back fallback_location: codes_path
       else
         render :edit, status: :unprocessable_entity
       end
@@ -11,6 +11,6 @@ class FavoritesController < ApplicationController
     def destroy
       @favorite = current_user.favorites.find_by(code_id: params[:code_id])
       @favorite.destroy!
-      redirect_to codes_path
+      redirect_back fallback_location: codes_path
     end
 end
