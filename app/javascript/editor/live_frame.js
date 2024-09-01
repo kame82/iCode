8318a86a1d27dcb6c005f7a79cc7a90362c9d99b
kame_mac <kametyaso@gmail.com>
2024-09-01 15:21:50 +0900

    fix: resolved to js error

--- app/javascript/editor/live_frame.js
+++ app/javascript/editor/live_frame.js
@@ -1,6 +1,6 @@
 // グローバルに関数を定義
+const live_frame = document.getElementById("live_frame");
 window.update_live_frame = function () {
-  const live_frame = document.getElementById("live_frame");
   const html_editor_value = document.getElementById("editorSource_HTML").value;
 
   if (live_frame && live_frame.contentWindow && live_frame.contentWindow.document) {
@@ -10,5 +10,8 @@ window.update_live_frame = function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   // 初回ロード時にエディタの内容をiframeに反映
+  if (!live_frame) {
+    return; // 要素が存在しない場合、処理を中断
+  }
   update_live_frame();
 });
