9ce51e11a6d0705b674286e7ada43b2e5c78d88e
kame_mac <kametyaso@gmail.com>
2024-09-07 14:51:04 +0900

    feat: add tailwind CSS to HTML editor

--- app/javascript/editor/editor.bundle.js
+++ app/javascript/editor/editor.bundle.js
@@ -28830,11 +28830,23 @@
 
    // グローバルに関数を定義
    const live_frame = document.getElementById("live_frame");
+
    window.update_live_frame = function () {
      const html_editor_value = document.getElementById("editorSource_HTML").value;
+     const css_editor_value = document.getElementById("editorSource_CSS").value;
 
      if (live_frame && live_frame.contentWindow && live_frame.contentWindow.document) {
        live_frame.contentWindow.document.body.innerHTML = html_editor_value;
+       live_frame.contentWindow.document.head.innerHTML +=
+         '<link rel="stylesheet" href="/assets/iframe.css"></link>';
+       live_frame.contentWindow.document.head.innerHTML += `<style>${css_editor_value}</style>`;
+       live_frame.contentWindow.document.documentElement.classList.add("light");
+       live_frame.contentWindow.document.body.classList.add(
+         "bg-white",
+         "border-2",
+         "border-customGray",
+         "p-4"
+       );
      }
    };
 
