0768a2e8a00db63eec0f14f5900ad9ff56328abb
kame_mac <kametyaso@gmail.com>
2024-09-11 23:40:11 +0900

    fix: fixed error and add pagenation

--- app/javascript/editor/copyClipboard.js
+++ app/javascript/editor/copyClipboard.js
@@ -1,38 +1,42 @@
 // clipboard API
 
-if (document.getElementById("copyBtn")) {
-  document.getElementById("copyBtn").addEventListener("click", function () {
-    const HTMLText = document.getElementById("HTML_tab");
-    const CSSText = document.getElementById("CSS_tab");
-    console.log(HTMLText.checked);
+function attachClipboardListener() {
+  if (document.getElementById("copyBtn")) {
+    document.getElementById("copyBtn").addEventListener("click", function () {
+      const HTMLText = document.getElementById("HTML_tab");
+      const CSSText = document.getElementById("CSS_tab");
 
-    let copyText;
-    if (HTMLText.checked == true) {
-      copyText = document.getElementById("editorSource_HTML").value;
-    } else if (CSSText.checked == true) {
-      copyText = document.getElementById("editorSource_CSS").value;
-    } else {
-      alert("Please select the language you want to copy");
-    }
-
-    const copyBtn = document.getElementById("copyBtn");
-    const copiedText = document.getElementById("copiedText");
-
-    navigator.clipboard.writeText(copyText).then(
-      () => {
-        copyBtn.classList.add("hidden");
-        copiedText.classList.remove("hidden");
-
-        window.setTimeout(() => {
-          copyBtn.classList.remove("hidden");
-          copiedText.classList.add("hidden");
-        }, 2200);
-      },
-      () => {
-        alert("Copy failed");
+      let copyText;
+      if (HTMLText.checked == true) {
+        copyText = document.getElementById("editorSource_HTML").value;
+      } else if (CSSText.checked == true) {
+        copyText = document.getElementById("editorSource_CSS").value;
+      } else {
+        alert("Please select the language you want to copy");
       }
-    );
-  });
-} else {
-  return false;
+
+      const copyBtn = document.getElementById("copyBtn");
+      const copiedText = document.getElementById("copiedText");
+
+      navigator.clipboard.writeText(copyText).then(
+        () => {
+          copyBtn.classList.add("hidden");
+          copiedText.classList.remove("hidden");
+
+          window.setTimeout(() => {
+            copyBtn.classList.remove("hidden");
+            copiedText.classList.add("hidden");
+          }, 2200);
+        },
+        () => {
+          alert("Copy failed");
+        }
+      );
+    });
+  } else {
+    return false;
+  }
 }
+
+document.addEventListener("turbo:load", attachClipboardListener);
+document.addEventListener("turbo:render", attachClipboardListener);
