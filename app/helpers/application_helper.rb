01690594f43ad2a4712ee31fff02ccebd162f892
kame_mac <kametyaso@gmail.com>
2024-09-12 23:21:10 +0900

    feat: add ogp tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,36 @@ module ApplicationHelper
     user_signed_in? && current_user.favorites.find_by(code_id: code.id).nil?
   end
 
+  def default_meta_tags
+    {
+      site: 'icode',
+      title: 'コードストックアプリ iCode',
+      # reverse: true,
+      separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
+      description: 'iCodeでは、あなたのコードを簡単に保存、管理、共有できます。',
+      keywords: 'icode,codestock',   #キーワードを「,」区切りで設定する
+      canonical: request.original_url,   #優先するurlを指定する
+      noindex: ! Rails.env.production?,
+      icon: [                    #favicon、apple用アイコンを指定する
+        { href: image_url('favicon.ico') },
+        # { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'icode',
+        title: 'コードストックアプリ iCode',
+        description: 'iCodeでは、あなたのコードを簡単に保存、管理、共有できます。',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('icodeOgp.webp'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+        # site: '@ツイッターのアカウント名',
+      }
+      # fb: {
+        # app_id: '自身のfacebookのapplication ID'
+      # }
+    }
+  end
 end
