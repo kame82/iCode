aea840591bf9c571d64faad5f0adad46b80f4d88
kame_mac <kametyaso@gmail.com>
2024-11-23 14:12:32 +0900

    fix: hidden the login button after login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,6 @@ class Users::SessionsController < Devise::SessionsController
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to codes_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
